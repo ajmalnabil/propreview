04352dbe82ea3efa66523223704cf13892efb368
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2017-12-27 14:33:28 +0800

    Added paperclip for image upload

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -70,6 +70,6 @@ class PropertiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def property_params
-      params.require(:property).permit(:name, :description, :builtup, :developer, :rating)
+      params.require(:property).permit(:name, :description, :builtup, :developer, :rating, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227051705) do
+ActiveRecord::Schema.define(version: 20171227060058) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20171227051705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
